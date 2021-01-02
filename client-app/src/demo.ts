8ec6758a2bb8ae70be72f901b0ddf926e4fa5f66
eric <eric@mcgillnet.net>
2021-01-02 16:34:31 -0500

    Before React & Typescript

---
+++ client-app/src/demo.ts
@@ -0,0 +1,25 @@
+let data = 42;
+
+data = 10;
+
+interface ICar {
+    color: string;
+    model: string;
+    topSpeed?: number;
+}
+
+const car1: ICar = {
+    color: 'blue',
+    model: 'BMW'
+}
+
+const car2: ICar = {
+    color: 'red',
+    model: 'Mercedes',
+    topSpeed: 100
+}
+
+const multiply = (x: number, y: number): string => {
+    return (x*y).toString();
+}
+
